98c0ffc5c108d5242091e76d186017ee5156699a
011912 <zhengyang@megabank.com.tw>
2022-09-08 16:23:25 +0800

    MergeTwoSortedLists

--- src/leetCode/easy/MergeTwoSortedLists.java
+++ src/leetCode/easy/MergeTwoSortedLists.java
@@ -1,5 +1,18 @@
 package leetCode.easy;
 
-public class MergeTwoSortedLists {
+import java.util.Arrays;
+import java.util.List;
 
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        List lsit1 = Arrays.asList(1, 2, 4);
+        List lsit2 = Arrays.asList(1, 3, 4);
+
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        return new ListNode();
+    }
 }
