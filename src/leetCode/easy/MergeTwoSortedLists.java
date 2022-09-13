68f1660004c78d478bb9512491f18e9835c31a73
011912 <zhengyang@megabank.com.tw>
2022-09-13 16:11:03 +0800

    MergeTwoSortedLists

--- src/leetCode/easy/MergeTwoSortedLists.java
+++ src/leetCode/easy/MergeTwoSortedLists.java
@@ -12,7 +12,21 @@ public class MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
-        return new ListNode();
+        ListNode list3;
+        if (list1.val < list2.val) {
+            list3 = list1;
+            list3.next = mergeTwoLists(list1.next, list2);
+        } else {
+            list3 = list2;
+            list3.next = mergeTwoLists(list1, list2.next);
+        }
+
+
+        return list3;
     }
+
+
 }
