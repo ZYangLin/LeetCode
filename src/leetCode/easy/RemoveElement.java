e6c7667cd1472ccafff6274f003672bcc6737cd5
011912 <zhengyang@megabank.com.tw>
2022-09-15 09:08:43 +0800

    1. RemoveElement.java 2. SearchInsertPosition.java

--- src/leetCode/easy/RemoveElement.java
+++ src/leetCode/easy/RemoveElement.java
@@ -1,4 +1,22 @@
 package leetCode.easy;
 
 public class RemoveElement {
+
+    public static void main(String[] args) {
+        RemoveElement re = new RemoveElement();
+        re.removeElement(3);
+    }
+
+    public int removeElement(int val) {
+        int i = 0;
+        int[] nums = {3, 2, 2, 3};
+
+        for (int num : nums) {
+            if (num != val) {
+                nums[i++] = num;
+            }
+        }
+
+        return i;
+    }
 }
---
+++ src/leetCode/easy/SearchInsertPosition.java
@@ -0,0 +1,8 @@
+package leetCode.easy;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        // 使用二分搜尋法
+
+    }
+}
