de73e88d74651de9b5ca75c8912ace72c27cc341
011912 <zhengyang@megabank.com.tw>
2022-09-19 11:03:55 +0800

    Add class 1. SearchInsertPosition.java.java

--- src/leetCode/easy/SearchInsertPosition.java
+++ src/leetCode/easy/SearchInsertPosition.java
@@ -1,8 +1,22 @@
 package leetCode.easy;
 
 public class SearchInsertPosition {
-    public int searchInsert(int[] nums, int target) {
-        // 使用二分搜尋法
+    public static int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
 
+        while (l < r) {
+            int m = l + (r - l) / 2;
+
+            if (nums[m] == target) return m;
+            else if (nums[m] > target) r = m;
+            else l = m + 1;
+        }
+        return nums[l] < target ? l + 1 : l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        searchInsert(nums, 5);
     }
 }
