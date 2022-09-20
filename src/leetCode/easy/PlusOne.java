de93e4babc8aefdc37815c93317f112cb758664f
011912 <zhengyang@megabank.com.tw>
2022-09-20 10:52:53 +0800

    add class 1. PlusOne.java

---
+++ src/leetCode/easy/PlusOne.java
@@ -0,0 +1,25 @@
+package leetCode.easy;
+
+public class PlusOne {
+
+    public static void main(String[] args) {
+        PlusOne po = new PlusOne();
+        int[] digits = {9, 1, 9};
+        po.plusOne(digits);
+    }
+
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] newDigits = new int[n+1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
